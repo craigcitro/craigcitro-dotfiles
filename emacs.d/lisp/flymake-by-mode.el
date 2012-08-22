a2e3d64f2635c16b17236694e766bbedff99fcd1
Craig Citro <craigcitro@gmail.com>
2012-08-22 09:25:16 -0700

    Let flymake create files inline (so that we can see local imports).

--- emacs.d/lisp/flymake-by-mode.el
+++ emacs.d/lisp/flymake-by-mode.el
@@ -4,7 +4,8 @@
 ;; Configure flymake for python
 (defun cc/flymake-pylint-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-with-folder-structure))
+                     'flymake-create-temp-inplace))
+                     ;; 'flymake-create-temp-with-folder-structure))
          (local-file (file-relative-name
                       temp-file
                       (file-name-directory buffer-file-name))))
