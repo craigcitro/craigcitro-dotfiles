3eedb5fa2e2806fd42dc6e17a0362aa65954ef6b
Craig Citro <craigcitro@gmail.com>
2012-11-17 23:49:09 -0800

    Update deft from elpa to HEAD.

--- emacs.d/elpa/deft-0.3/deft-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; deft-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (deft) "deft" "deft.el" (20610 16207))
-;;; Generated autoloads from deft.el
-
-(autoload 'deft "deft" "\
-Switch to *Deft* buffer and load files.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("deft-pkg.el") (20610 16207 309973))
-
-;;;***
-
-(provide 'deft-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; deft-autoloads.el ends here
