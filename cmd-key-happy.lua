2a2ee4fd38ced4711844ecd3c833a92f3e026f8f
Craig Citro <craigcitro@gmail.com>
2013-01-22 21:30:26 -0800

    Massively overhaul cmd-key-happy configs, and some iterm2 updates.

--- cmd-key-happy.lua
+++ cmd-key-happy.lua
@@ -1,11 +1,12 @@
 function Set(t)
    local s = {}
+   for a,b in pairs(t) do print(a, b) end
    for _,v in pairs(t) do s[v] = true end
    return s
 end
 
 function set_contains(t, e)
-   return t[e]
+   return t[e] or false
 end
 
 -- The set of global shortcuts we don't want to swap cmd/alt. For these,
@@ -14,13 +15,6 @@ end
 
 global_excludes = Set{
                        -- "alt-cmd-left",
-                       -- "alt-cmd-right",
-                       -- "alt-cmd-down",
-                       -- "alt-cmd-1",
-                       -- "alt-cmd-2",
-                       -- "alt-cmd-3",
-                       -- "alt-cmd-4",
-                       -- "cmd-,",
                        "cmd-tab",
                        "shift-cmd-tab",
 	               "cmd-h",
@@ -31,11 +25,12 @@ global_excludes = Set{
 -- normal OS Terminal behaviour. If you omit items then you would
 -- have to use "alt-w" to close a terminal window.
 
-apps = {
-   -- XQuartz doesn't have any special rules.
-   X11 = { exclude = { } },
-   -- NX annoyingly has its own name.
-   ["NX Player for OS X"] = { exclude = { } },
+chrome_swaps = Set{
+  "cmd-v",
+  "cmd-w",
+  "cmd-z", "cmd-b", "cmd-f",
+  "cmd-/", "cmd-5", "cmd-t",
+  "shift-cmd-.", "shift-cmd-,",
 }
 
 -- Return true to swap cmd/alt, otherwise false.
@@ -55,22 +50,15 @@ apps = {
 --   shift control alt cmd fn, separated by a hyphen ("-").
 
 function swap_keys(t)
-   -- for i,v in pairs(t) do print(i,v) end
-   -- print(t.appname)
-   -- local file = assert(io.open("/Users/craigcitro/something.txt", "w"))
-   -- file:write(t.appname)
-   -- file:write("\n")
-   -- file:close()
    if set_contains(global_excludes, t.key_str_seq) then
       return false
    end
-   if not apps[t.appname] then
+   if not (t.appname == "Google Chrome") then
       return false
    end
-   local excludes = apps[t.appname]["exclude"]
-   if set_contains(excludes, t.key_str_seq) then
-      -- print("exluding: ", t.key_str_seq)
-      return false
+   if set_contains(chrome_swaps, t.key_str_seq) or
+      set_contains(chrome_swaps, t.key_str_seq:gsub("alt%-", "cmd-")) then
+      return true
    end
-   return true
+   return false
 end
