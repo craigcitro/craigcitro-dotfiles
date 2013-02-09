bdb4d458acb44757f08bc6122276166ad0c3c1e1
Craig Citro <craigcitro@gmail.com>
2013-02-08 20:31:52 -0800

    Cleanup config.py to be shorter and awesomer.

--- config.py
+++ config.py
@@ -1,47 +1,46 @@
 #!/usr/bin/env python
-#
-# config.py
-#
-# Basic script to do some machine config
-#
+"""Basic script to do some machine config."""
 
-import getopt
+import contextlib
 import os
-import platform
 import subprocess
-import sys
-
-HOMEDIR = os.path.expanduser('~')
 
 BASIC_LINKS = [
     # General
-    'Xdefaults',
-    'bash_profile',
-    'bashrc',
-    'gitconfig',
-    'gitignore',
-    'hgrc',
-    'inputrc',
-    'screenrc',
-    'tmux.conf',
+    'Xdefaults', 'bash_profile', 'bashrc', 'gitconfig', 'gitignore', 'hgrc',
+    'inputrc', 'screenrc', 'tmux.conf',
     # Editors
-    'emacs',
-    'emacs.d',
-    'vimrc',
+    'emacs', 'emacs.d', 'vimrc',
     # Languages
-    'Rprofile',
-    'ghci',
-    'pdbrc',
-    'pdbrc.py',
-    'pythonrc',
-    ]
-MAC_LINKS = []
-XWINDOWS_LINKS = [
-    'xinit',
+    'Rprofile', 'ghci', 'pdbrc', 'pdbrc.py', 'pythonrc',
     ]
 
 
-SCRIPTS = []
+@contextlib.contextmanager
+def Chdir(path):
+  current_dir = os.getcwd()
+  print ' +++ Changing directory to %s +++' % path
+  os.chdir(path)
+  yield
+  print ' --- Returning to directory to %s ---' % current_dir
+  os.chdir(current_dir)
+
+
+def _CreateLink(filename, linkname):
+  """Link filename to linkname, returning True on success."""
+  print "== Symlinking %s to %s" % (filename, linkname)
+  if os.path.exists(linkname):
+    print 'File %s already exists, skipping.' % (linkname,)
+    return True
+  if os.path.lexists(linkname):
+    print '    >> Removing broken symlink %s <<' % (linkname,)
+    os.remove(linkname)
+  try:
+    subprocess.check_call(['ln', '-s', filename, linkname])
+  except subprocess.CalledProcessError, e:
+    print "ERROR creating symbolic link: " + str(e)
+    return False
+  return True
 
 
 def _SetupLinks(links_to_create):
@@ -49,57 +48,25 @@ def _SetupLinks(links_to_create):
   print "Creating symbolic links ..."
   print
   repodir = os.getcwd()
-  os.chdir(HOMEDIR)
-  for f in links_to_create:
-    filename = os.path.join(repodir, f)
-    linkname = '.' + f
-    print "--- Symlinking %s to %s" % (filename, linkname)
-    if os.path.exists(linkname):
-      print 'File %s already exists, skipping.' % (linkname,)
-      continue
-    elif os.path.lexists(linkname):
-      print '    >> Removing broken symlink %s <<' % (linkname,)
-      os.remove(linkname)
-    try:
-      subprocess.check_call(['ln', '-s', filename, linkname])
-    except subprocess.CalledProcessError, e:
-      print "ERROR creating symbolic link: " + str(e)
-      break
-  os.chdir(repodir)
-  return
-
-
-def _RunScripts(scripts):
-  print
-  print "Running scripts ..."
-  print
-  for s in scripts:
-    print "=== Running: %s " % (' '.join(s),)
-    try:
-      subprocess.check_call(s)
-    except subprocess.CalledProcessError, e:
-      print "ERROR running script: " + str(e)
-      break
+  homedir = os.path.expanduser('~')
+  with Chdir(homedir):
+    for f in links_to_create:
+      filename = os.path.join(repodir, f)
+      linkname = '.' + f
+      if not _CreateLink(filename, linkname):
+        break
+  # Now the binaries
+  binary_dest_dir = os.path.join(homedir, 'bin')
+  if not os.path.exists(binary_dest_dir):
+    os.mkdir(binary_dest_dir)
+  with Chdir(binary_dest_dir):
+    binary_src_dir = os.path.join(repodir, 'bin')
+    for binary in os.listdir(binary_src_dir):
+      filename = os.path.join(binary_src_dir, binary)
+      if not _CreateLink(filename, binary):
+        break
 
 
 if __name__ == '__main__':
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], 'x', ['with-x'])
-    opts = [x[0] for x in opts]
-  except getopt.GetoptError, e:
-    print "Error parsing arguments: " + str(e)
-    exit(1)
-  setup_x = False
-  if ('-x' in opts) or ('--with-x' in opts):
-    setup_x = True
-
-  links_to_create = BASIC_LINKS
-  if platform.system() == 'Darwin':
-    links_to_create += MAC_LINKS
-  elif platform.system() == 'Linux':
-    if setup_x:
-      links_to_create += XWINDOWS_LINKS
-
-  _SetupLinks(links_to_create)
-  print "Don't forget to link something to .tmux.conf.colors."
-  _RunScripts(SCRIPTS)
+  _SetupLinks(BASIC_LINKS)
+  print "Don't forget to link something to .tmux.colors.conf."
