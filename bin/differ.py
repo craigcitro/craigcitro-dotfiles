11b63741b2f9bc82aefb9f58dd1bce24b005c6f5
Craig Citro <craigcitro@google.com>
2013-02-25 00:07:52 -0800

    Cleanup differ.py once I remembered `with NamedTemporaryFile ...`.

--- bin/differ.py
+++ bin/differ.py
@@ -9,18 +9,18 @@ def main(argv):
   diff = os.environ.get('DIFFER', 'colordiff')
   diff_opts = argv[1]
   args = argv[2:]
-  out = tempfile.NamedTemporaryFile()
-  while args:
-    if len(args) < 3:
-      break
-    _, left, right = args[:3]
-    args = args[3:]
-    subprocess.call([diff, diff_opts, left, right], stdout=out, stderr=out)
+  with tempfile.NamedTemporaryFile() as out:
+    print 'out.name: ', out.name
+    while args:
+      if len(args) < 3:
+        break
+      _, left, right = args[:3]
+      args = args[3:]
+      subprocess.call([diff, diff_opts, left, right], stdout=out, stderr=out)
 
-  out.seek(0)
-  pager = os.environ.get('PAGER', 'less -qFRX')
-  subprocess.call(pager.split() + [out.name])
-  out.close()
+    out.seek(0)
+    pager = os.environ.get('PAGER', 'less -qFRX')
+    subprocess.call(pager.split() + [out.name])
 
 
 if __name__ == '__main__':
