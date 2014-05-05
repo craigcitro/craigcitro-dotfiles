1e62193c3c7a8c1fe91695b63a5ac50d7db16927
Craig Citro <craigcitro@google.com>
2014-05-04 18:15:28 -0700

    Add new emacs packages, small org tweaks.

---
+++ emacs.d/elpa/gitconfig-1.0.0/gitconfig-autoloads.el
@@ -0,0 +1,18 @@
+;;; gitconfig-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("gitconfig-pkg.el" "gitconfig.el") (21350
+;;;;;;  47200 3611 0))
+
+;;;***
+
+(provide 'gitconfig-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; gitconfig-autoloads.el ends here
---
+++ emacs.d/elpa/gitconfig-1.0.0/gitconfig-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gitconfig" "1.0.0" "Emacs lisp interface to work with git-config variables" (quote nil))
