8e21cd73a1924611149a53b1ae05a8146fd70747
Craig Citro <craigcitro@google.com>
2015-04-29 17:43:55 -0700

    update emacs packages

---
+++ emacs.d/elpa/flycheck-google-cpplint-20140806.925/flycheck-google-cpplint-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-google-cpplint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flycheck-google-cpplint.el") (21825 28013
+;;;;;;  31392 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-google-cpplint-autoloads.el ends here
---
+++ emacs.d/elpa/flycheck-google-cpplint-20140806.925/flycheck-google-cpplint-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck-google-cpplint" "20140806.925" "Help to comply with the Google C++ Style Guide" '((flycheck "0.20alpha1")) :url "https://github.com/flycheck/flycheck-google-cpplint/" :keywords '("flycheck" "c" "c++"))
