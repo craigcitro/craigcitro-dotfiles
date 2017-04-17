a5c668a25e2f8865003a49356bdcad3375913fa2
Craig Citro <craigcitro@google.com>
2017-04-17 08:40:07 -0700

    Silly formatting for .py files.

--- bin/differ.py
+++ bin/differ.py
@@ -5,29 +5,31 @@ import subprocess
 import sys
 import tempfile
 
-def main(argv):
-  diff = os.environ.get('DIFFER', 'colordiff')
-  with open(os.devnull, 'w') as f:
-    exists = subprocess.call([diff, '-v'], stdout=f, stderr=f)
-  if exists:
-    print 'Diff program %s not found'.format(diff)
-    return 1
-  args = argv[1:]
-  diff_opts = '-u' if args[0] == ':' else args.pop(0)
-  with tempfile.NamedTemporaryFile() as out:
-    while args:
-      if len(args) < 2:
-        break
-      if args[0] == ':':
-        args.pop(0)
-      left, right = args[:2]
-      args = args[2:]
-      subprocess.call([diff, diff_opts, left, right], stdout=out, stderr=out)
 
-    out.seek(0)
-    pager = os.environ.get('PAGER', 'less -qFRX')
-    subprocess.call(pager.split() + [out.name])
+def main(argv):
+    diff = os.environ.get('DIFFER', 'colordiff')
+    with open(os.devnull, 'w') as f:
+        exists = subprocess.call([diff, '-v'], stdout=f, stderr=f)
+    if exists:
+        print 'Diff program %s not found'.format(diff)
+        return 1
+    args = argv[1:]
+    diff_opts = '-u' if args[0] == ':' else args.pop(0)
+    with tempfile.NamedTemporaryFile() as out:
+        while args:
+            if len(args) < 2:
+                break
+            if args[0] == ':':
+                args.pop(0)
+            left, right = args[:2]
+            args = args[2:]
+            subprocess.call(
+                [diff, diff_opts, left, right], stdout=out, stderr=out)
+
+        out.seek(0)
+        pager = os.environ.get('PAGER', 'less -qFRX')
+        subprocess.call(pager.split() + [out.name])
 
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
--- config.py
+++ config.py
@@ -8,76 +8,90 @@ import subprocess
 
 BASIC_LINKS = [
     # General
-    'Xdefaults', 'bash_profile', 'bashrc', 'colordiffrc', 'gitconfig',
-    'gitignore', 'hgrc', 'inputrc', 'screenrc', 'tmux.conf',
+    'Xdefaults',
+    'bash_profile',
+    'bashrc',
+    'colordiffrc',
+    'gitconfig',
+    'gitignore',
+    'hgrc',
+    'inputrc',
+    'screenrc',
+    'tmux.conf',
     # Editors
-    'emacs', 'emacs.d', 'vimrc',
+    'emacs',
+    'emacs.d',
+    'vimrc',
     # Languages
-    'pdbrc', 'pdbrc.py', 'pythonrc', 'pylintrc',
-    'Rprofile', 'ghci',
-    ]
+    'pdbrc',
+    'pdbrc.py',
+    'pythonrc',
+    'pylintrc',
+    'Rprofile',
+    'ghci',
+]
 
 
 @contextlib.contextmanager
 def Chdir(path):
-  current_dir = os.getcwd()
-  print ' +++ Changing directory to %s +++' % path
-  os.chdir(path)
-  yield
-  print ' --- Returning to directory to %s ---' % current_dir
-  print
-  os.chdir(current_dir)
+    current_dir = os.getcwd()
+    print ' +++ Changing directory to %s +++' % path
+    os.chdir(path)
+    yield
+    print ' --- Returning to directory to %s ---' % current_dir
+    print
+    os.chdir(current_dir)
 
 
 def _CreateLink(filename, linkname, indent='    '):
-  """Link filename to linkname, returning True on success."""
-  print '%s== Symlinking %s to %s' % (indent, filename, linkname)
-  if os.path.exists(linkname):
-    print '%sFile %s already exists, skipping.' % (indent, linkname)
+    """Link filename to linkname, returning True on success."""
+    print '%s== Symlinking %s to %s' % (indent, filename, linkname)
+    if os.path.exists(linkname):
+        print '%sFile %s already exists, skipping.' % (indent, linkname)
+        return True
+    if os.path.lexists(linkname):
+        print '%s>> Removing broken symlink %s <<' % (indent, linkname)
+        os.remove(linkname)
+    try:
+        subprocess.check_call(['ln', '-s', filename, linkname])
+    except subprocess.CalledProcessError, e:
+        print '%sERROR creating symbolic link: %s' % (indent, e)
+        return False
     return True
-  if os.path.lexists(linkname):
-    print '%s>> Removing broken symlink %s <<' % (indent, linkname)
-    os.remove(linkname)
-  try:
-    subprocess.check_call(['ln', '-s', filename, linkname])
-  except subprocess.CalledProcessError, e:
-    print '%sERROR creating symbolic link: %s' % (indent, e)
-    return False
-  return True
 
 
 def _SetupLinks(links_to_create):
-  print
-  print 'Creating symbolic links ...'
-  print
-  repodir = os.getcwd()
-  homedir = os.path.expanduser('~')
-  # Link the dotfiles
-  with Chdir(homedir):
-    for f in links_to_create:
-      filename = os.path.join(repodir, f)
-      linkname = '.' + f
-      if not _CreateLink(filename, linkname):
-        break
-  # Now the binaries
-  binary_dest_dir = os.path.join(homedir, 'bin')
-  if not os.path.exists(binary_dest_dir):
-    os.mkdir(binary_dest_dir)
-  with Chdir(binary_dest_dir):
-    binary_src_dir = os.path.join(repodir, 'bin')
-    for binary in os.listdir(binary_src_dir):
-      filename = os.path.join(binary_src_dir, binary)
-      if not _CreateLink(filename, binary):
-        break
-  # Don't forget a tmux colors file
-  if 'google.com' in socket.gethostname():
-    tmux_colors = 'tmux.green.conf'
-  else:
-    tmux_colors = 'tmux.blue.conf'
-  tmux_colors = os.path.join(repodir, tmux_colors)
-  with Chdir(homedir):
-    _CreateLink(tmux_colors, '.tmux.colors.conf')
+    print
+    print 'Creating symbolic links ...'
+    print
+    repodir = os.getcwd()
+    homedir = os.path.expanduser('~')
+    # Link the dotfiles
+    with Chdir(homedir):
+        for f in links_to_create:
+            filename = os.path.join(repodir, f)
+            linkname = '.' + f
+            if not _CreateLink(filename, linkname):
+                break
+    # Now the binaries
+    binary_dest_dir = os.path.join(homedir, 'bin')
+    if not os.path.exists(binary_dest_dir):
+        os.mkdir(binary_dest_dir)
+    with Chdir(binary_dest_dir):
+        binary_src_dir = os.path.join(repodir, 'bin')
+        for binary in os.listdir(binary_src_dir):
+            filename = os.path.join(binary_src_dir, binary)
+            if not _CreateLink(filename, binary):
+                break
+    # Don't forget a tmux colors file
+    if 'google.com' in socket.gethostname():
+        tmux_colors = 'tmux.green.conf'
+    else:
+        tmux_colors = 'tmux.blue.conf'
+    tmux_colors = os.path.join(repodir, tmux_colors)
+    with Chdir(homedir):
+        _CreateLink(tmux_colors, '.tmux.colors.conf')
 
 
 if __name__ == '__main__':
-  _SetupLinks(BASIC_LINKS)
+    _SetupLinks(BASIC_LINKS)
