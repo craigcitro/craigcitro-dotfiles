5574abc2e0d6edc838c9ae23668e08ae8bfb7c3c
Craig Citro <craigcitro@google.com>
2023-10-09 09:01:07 -0700

    package updates

---
+++ emacs.d/elpa/cython-mode-20221130.1257/cython-mode-pkg.el
@@ -0,0 +1,4 @@
+(define-package "cython-mode" "20221130.1257" "Major mode for editing Cython files" 'nil :commit "3e4790559d3168fe992cf2aa62f01423038cedb5")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/lean-mode-20230611.728/lean-mode-autoloads.el
@@ -0,0 +1,147 @@
+;;; lean-mode-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "lean-debug" "lean-debug.el" (0 0 0 0))
+;;; Generated autoloads from lean-debug.el
+
+(register-definition-prefixes "lean-debug" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-dev" "lean-dev.el" (0 0 0 0))
+;;; Generated autoloads from lean-dev.el
+
+(register-definition-prefixes "lean-dev" '("lean-diff-test-file"))
+
+;;;***
+
+;;;### (autoloads nil "lean-eri" "lean-eri.el" (0 0 0 0))
+;;; Generated autoloads from lean-eri.el
+
+(register-definition-prefixes "lean-eri" '("lean-eri-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-flycheck" "lean-flycheck.el" (0 0 0 0))
+;;; Generated autoloads from lean-flycheck.el
+
+(register-definition-prefixes "lean-flycheck" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-hole" "lean-hole.el" (0 0 0 0))
+;;; Generated autoloads from lean-hole.el
+
+(register-definition-prefixes "lean-hole" '("lean-hole"))
+
+;;;***
+
+;;;### (autoloads nil "lean-info" "lean-info.el" (0 0 0 0))
+;;; Generated autoloads from lean-info.el
+
+(autoload 'lean-info-mode "lean-info" "\
+Major mode for Lean Info Buffer
+
+\(fn)" t nil)
+
+(register-definition-prefixes "lean-info" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-input" "lean-input.el" (0 0 0 0))
+;;; Generated autoloads from lean-input.el
+
+(register-definition-prefixes "lean-input" '("lean-input-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-leanpkg" "lean-leanpkg.el" (0 0 0 0))
+;;; Generated autoloads from lean-leanpkg.el
+
+(register-definition-prefixes "lean-leanpkg" '("lean-leanpkg-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-message-boxes" "lean-message-boxes.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from lean-message-boxes.el
+
+(register-definition-prefixes "lean-message-boxes" '("lean-message-boxes-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-mode" "lean-mode.el" (0 0 0 0))
+;;; Generated autoloads from lean-mode.el
+
+(autoload 'lean-mode "lean-mode" "\
+Major mode for Lean
+     \\{lean-mode-map}
+Invokes `lean-mode-hook'.
+
+\(fn)" t nil)
+
+(push '("\\.lean$" . lean-mode) auto-mode-alist)
+
+(register-definition-prefixes "lean-mode" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-right-click" "lean-right-click.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from lean-right-click.el
+
+(register-definition-prefixes "lean-right-click" '("lean-right-click-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-server" "lean-server.el" (0 0 0 0))
+;;; Generated autoloads from lean-server.el
+
+(register-definition-prefixes "lean-server" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-settings" "lean-settings.el" (0 0 0 0))
+;;; Generated autoloads from lean-settings.el
+
+(register-definition-prefixes "lean-settings" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-syntax" "lean-syntax.el" (0 0 0 0))
+;;; Generated autoloads from lean-syntax.el
+
+(register-definition-prefixes "lean-syntax" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-type" "lean-type.el" (0 0 0 0))
+;;; Generated autoloads from lean-type.el
+
+(register-definition-prefixes "lean-type" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil "lean-util" "lean-util.el" (0 0 0 0))
+;;; Generated autoloads from lean-util.el
+
+(register-definition-prefixes "lean-util" '("lean-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("lean-mode-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lean-mode-autoloads.el ends here
